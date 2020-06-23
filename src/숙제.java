95ebac6d6da7e47bb0831f513b4b5319f7437a4e
WanDooGong <yachtboatfor@gmail.com>
2020-06-23 20:06:41 +0900

    200623 화 수업이다.

--- src/숙제.java
+++ src/숙제.java
@@ -10,14 +10,78 @@ class Vehicle {
     int v_speed;
 }
 
-class Adding {
+class Cust_Adding {
 
+    int sum = 0;
+    int a;
+    int b;
+
+    public Cust_Adding() {
+        a = 1;
+        b = 20;
+    }
+
+    public Cust_Adding(int num1, int num2) {
+
+        a = num1;
+        b = num2;
+    }
+
+    public void adding() {
+        for(int i = a; i <= b; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+                //System.out.println(sum);
+            }
+            //System.out.println(sum);
+        }
+
+    }
 }
 
 public class 숙제 {
 
+    int a, b;
+
+    숙제(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void adding() {
+
+        int sum = 0;
+
+        for(int i = a; i <= b; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+                //System.out.println(sum);
+            }
+            //System.out.println(sum);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("숙제이다");
+
+        숙제 a = new 숙제();
+        a.adding();
+
+
+        /*Cust_Adding ca2 = new Cust_Adding(1, 20);
+        ca2.adding();*/
+
+        /*int sum=0;
+
+        for(int i=1; i<=20; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+                //System.out.println(sum);
+            }
+            //System.out.println(sum);
+
+        }
+        System.out.println(sum);*/
     }
 
 }
